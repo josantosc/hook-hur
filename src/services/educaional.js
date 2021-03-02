3800cbebc61e13a5eb1d6eea759049fd4dcdc149
Joeckson <joeckson.s@gmail.com>
2021-03-02 11:19:05 -0300

    feature: added route services of educacional

---
+++ src/services/educaional.js
@@ -0,0 +1,29 @@
+const { apiEducacional } = require("./apis");
+const { EVENTS, PARAMETERS, ROUTES } = require("../utils/constantes");
+const { buildEvent } = require("../utils/intents");
+const {
+  buildTemplate,
+  mountResponse,
+} = require("../utils/educacional/format-resp-post");
+
+const getQuestions = async (req, res) => {
+  const params = req.body.queryResult.parameters;
+
+  const responseQuestions = Object.values(params);
+
+  const { data } = await apiEducacional.get(`${ROUTES.BASE}`);
+
+  const resultQuestions = buildTemplate(data, responseQuestions);
+
+  const formarToDialogflow = mountResponse(resultQuestions);
+
+  const paramtrs = {
+    [PARAMETERS.QUESTIONS]: formarToDialogflow,
+  };
+
+  return buildEvent(EVENTS.SHOW, paramtrs);
+};
+
+const postQuestions = async (req, res) => {};
+
+module.exports = { getQuestions, postQuestions };
