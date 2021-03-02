bb91740b4c0d98c59b90e8b7e24618cf2e7954a6
Joeckson <joeckson.s@gmail.com>
2021-03-02 13:14:06 -0300

    refactore: obs

--- src/utils/sample/map-intent-sample.js
+++ src/utils/sample/map-intent-sample.js
@@ -2,9 +2,6 @@ const { INTENTS } = require("../constantes");
 const { SAMPLE } = INTENTS;
 const { getSample, postSample } = require("../../services/sample");
 
-const mappedIntents = {
-  [SAMPLE.INTENT_GET_FOFOCA]: getSample,
-  [SAMPLE.INTENT_POST_FOFOCA]: postSample,
-};
+const mappedIntents = {};
 
 module.exports = { mappedIntents };
